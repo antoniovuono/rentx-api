e99d5e21be4d9649068f4cb71de512fdfd15169b
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-07-23 18:32:28 -0300

    categorie created routes configured

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/cetegories.routes";
+
 const app = express();
 
-app.get("/", (request, response) => {
-  const message1 = "Bem vindo ao rentx";
-  return response.json({ message: message1 });
-});
+app.use(express.json());
+
+app.use(categoriesRoutes);
 
 app.listen(3333, () =>
   console.log({ message: "Server is ruining on port 3333" })
