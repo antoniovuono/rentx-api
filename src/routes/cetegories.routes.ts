8bb29afe88318bf38c49368c67edca6d52c42b5e
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-07-23 19:16:19 -0300

    adding typescript to categories

--- src/routes/cetegories.routes.ts
+++ src/routes/cetegories.routes.ts
@@ -1,18 +1,25 @@
 import { Router } from "express";
 
+import { Category } from "../models/Category";
+
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
+    created_at: new Date(),
   });
 
-  return response.status(201).send();
+  categories.push(category);
+
+  return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
