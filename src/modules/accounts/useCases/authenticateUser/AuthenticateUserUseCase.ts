e0cd7545c715984189bdf5d2cde7fa4ca33bc03c
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-07-30 13:54:46 -0300

    Authentication developed

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,56 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findUserByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect");
+    }
+    const isPasswordCorrect = await compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      throw new Error("Email or password incorrect");
+    }
+
+    const token = sign({}, "d041ef5adef06de7192e919281268875", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const authenticateSessionResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return authenticateSessionResponse;
+  }
+}
+
+export { AuthenticateUserUseCase };
