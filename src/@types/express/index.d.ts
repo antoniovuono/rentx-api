c6f57a249110a2fc9dfe08ea7aa3bf3289ad123a
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-08-01 10:00:28 -0300

    Trying to resolve the update avatar fix

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,5 +1,5 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 declare namespace Express {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   export interface Request {
     user: {
       id: string;
