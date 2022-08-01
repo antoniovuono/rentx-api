55d20d408ecc1d7fb8abc4fa170d7bf285edbaf1
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-08-01 15:35:31 -0300

    Removing a eslint rule

--- src/modules/cars/useCases/createCategory/tests/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/tests/CreateCategoryUseCase.spec.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/extensions
 import { AppError } from "../../../../../errors/AppError";
 import { InMemoryCategoriesRepository } from "../../../repositories/in-memory/InMemoryCategoriesRepository";
 import { CreateCategoryUseCase } from "../CreateCategoryUseCase";
