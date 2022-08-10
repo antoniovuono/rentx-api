5fd654dc3e7e66ba8871e36569ce8fe42202fed5
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-08-10 14:18:01 -0300

    Cars repository created

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,5 +1,14 @@
-import { Column, Entity, PrimaryColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
 import { v4 as uuidv4 } from "uuid";
+
+import { Category } from "./Category";
 
 @Entity("cars")
 class Car {
@@ -16,7 +25,7 @@ class Car {
   daily_rate: number;
 
   @Column()
-  available: boolean;
+  available = true;
 
   @Column()
   license_plate: string;
@@ -27,17 +36,19 @@ class Car {
   @Column()
   brand: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: "category_id" })
+  category: Category;
+
   @Column()
   category_id: string;
 
-  @Column()
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuidv4();
-      this.available = true;
-      this.created_at = new Date();
     }
   }
 }
--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -1,3 +1,44 @@
-class CarsRepository {}
+import { Repository } from "typeorm";
+
+import { AppDataSource } from "../../../../../shared/infra/typeorm";
+import { ICreateCarDTO } from "../../../dtos/ICreateCarDTO";
+import { ICarsRepository } from "../../../repositories/ICarsRepository";
+import { Car } from "../entities/Car";
+
+class CarsRepository implements ICarsRepository {
+  private repository: Repository<Car>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(Car);
+  }
+
+  async create({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    fine_amount,
+    brand,
+    category_id,
+  }: ICreateCarDTO): Promise<Car> {
+    const car = this.repository.create({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+
+    await this.repository.save(car);
+
+    return car;
+  }
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    const car = await this.repository.findOneBy({ license_plate });
+    return car;
+  }
+}
 
 export { CarsRepository };
