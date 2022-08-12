f691e079b6c8720913a59714d57218905c646e1b
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-08-12 10:00:49 -0300

    middleware added to other routes like a categories and specifications

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
 
 import { CreateSpecificationController } from "../../../../modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router();
 
 const createSpecificationsController = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post("/", createSpecificationsController.handle);
+specificationsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationsController.handle
+);
 
 export { specificationsRoutes };
