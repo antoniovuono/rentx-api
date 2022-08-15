c6a8b9252e701cf04aa9aba1fbf9cfcd02922b97
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-08-15 15:13:11 -0300

    Cars list developed and fineshed

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
 import { CreateCarController } from "../../../../modules/cars/useCases/createCar/CreateCarController";
+import { ListCarsController } from "../../../../modules/cars/useCases/listCars/ListCarsController";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listCarsController = new ListCarsController();
 
 carsRoutes.post(
   "/",
@@ -14,5 +16,7 @@ carsRoutes.post(
   ensureAdmin,
   createCarController.handle
 );
+
+carsRoutes.get("/available", listCarsController.handle);
 
 export { carsRoutes };
