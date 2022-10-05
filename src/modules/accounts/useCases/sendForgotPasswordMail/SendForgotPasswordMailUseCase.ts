cd24dbe716c3d208bdb40201d949972e0c8d739a
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-10-05 14:15:11 -0300

    send forgot e-mail use case started

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -0,0 +1,39 @@
+import { inject, injectable } from "tsyringe";
+import { v4 as uuidv4 } from "uuid";
+
+import { IDateProvider } from "../../../../shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "../../../../shared/errors/AppError";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { IUsersTokensRepository } from "../../repositories/IUsersTokensRepository";
+
+@injectable()
+class SendForgotPasswordMailUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject("DayjsDateProvider")
+    private dayjsDateProvider: IDateProvider
+  ) {}
+
+  async execute(email: string) {
+    const user = await this.usersRepository.findUserByEmail(email);
+
+    if (!user) {
+      throw new AppError("Users does not exists");
+    }
+
+    const token = uuidv4();
+
+    const expires_date = this.dayjsDateProvider.addHours(3);
+
+    await this.usersTokensRepository.create({
+      refresh_token: token,
+      user_id: user.id,
+      expires_date,
+    });
+  }
+}
+
+export { SendForgotPasswordMailUseCase };
